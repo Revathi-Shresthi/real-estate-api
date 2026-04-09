965b81ab5d54c3860e9fd920ab6e7f6b81c73abe
Revathi-Shresthi <revathishresthi703@gmail.com>
2026-04-09 18:04:48 +0530

    chapter 9 complete - cicd pipelines done

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    ignores: [
+      'node_modules/**',
+      'coverage/**',
+      'drizzle/**',
+    ],
+  },
+  {
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+      'semi': ['error', 'always'],
+      'quotes': ['error', 'single'],
+    },
+  },
+];
